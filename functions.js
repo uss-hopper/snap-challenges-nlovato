ab3328358e3e51f7f8320fa71d19d9c4dcad52eb
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-09 16:24:03 -0600

    fixing git config

---
+++ functions.js
@@ -0,0 +1,52 @@
+/**
+ * SnapChallenge 04_09 I.E.
+ *
+ *Take two numbers and mjultiply them.
+ *
+ *@param int firstNumber One mumber to multiply.
+ *@param int secondNumber The other number to multiply.
+ *@return int The product of the two numbers.
+ */
+function multiplyNumbers (firstNumer, secondNumber) {
+	return firstnumber * secondNumber;
+}
+
+let myNumber = -5;
+let firstNumber = 29;
+
+
+console.log(firstNumber);
+
+
+
+console.log(multiplyNumbers(-2, myNumber));
+console.log(multiplyNumbers(2, myNumber));
+console.log(multiplyNumbers(-1, 5));
+console.log(multiplyNumbers(6, 487635789));
+
+console.log(firstNumber);
+
+/**
+ * Function string I.E.
+ *
+ *Take two names and concatenate them.
+ *
+ * @param string firstName The given name of cocatenate.
+ * @param string lastName The family name to concatenate.
+ * @return string The full name of our person.
+ */
+
+function getFullName (firstName, lastName) {
+	return firstName + ' ' + lastName;
+}
+
+let myFirstName = 'Barry'
+
+console.log(getFullName(myFirstName, 'Schulzetenberg'));
+console.log(getFullName("Paul", "Schulzetenberg"));
+
+myFirstName = 'Susanna';
+
+console.log(getFullName(myFirstName, 'Schulzetenberg'));
+
+console.log(getFullName(myFirstName, 'Schulzetenberg'));
--- snap_04_09.js
+++ snap_04_09.js
@@ -1,5 +1,5 @@
-let number1=5
-let number2=2
+let number1 = 5
+let number2 = 2
 
 function add(number1, number2){
 	return number1 = number2
