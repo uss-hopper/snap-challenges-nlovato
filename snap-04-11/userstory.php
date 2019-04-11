5fb89a12dab2d4e1f4126091820f30f95597772f
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-11 09:08:07 -0600

    Added Snap-04-11 user story

---
+++ snap-04-11/userstory.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Snap Challenge User Story</title>
+	</head>
+
+	<body>
+
+		<h1>Snap Challenge User Story</h1>
+
+		<br>
+		<br>
+
+		<p>As a blogger user I can use mailchimp to learn how to edit images.</p>
+
+		<br>
+		<br>
+		<br>
+
+
+
+	</body>
+
+</html>
