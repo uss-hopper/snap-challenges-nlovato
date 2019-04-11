a1f1a4d521e31192b0307cbe1e1b4850e4466f7b
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-11 09:21:09 -0600

    Added erd diagram

--- snap-04-11/erd.php
+++ snap-04-11/erd.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
 
+	<head>
+		<meta charset="UTF-8">
+		<title>Data-Design</title>
+
+		<head>
+			<meta charset="UTF-8">
+			<title>Snap Challenge User Story</title>
+		</head>
+
+	<body>
+
+		<h1>Snap Challenge User Story</h1>
+
+		<br>
+		<br>
+
+		img src="erdsnapchallenge.png" width=" 250px" alt="Harley Quinzel">
+
+	</body>
--- snap-04-11/userstory.php
+++ snap-04-11/userstory.php
@@ -19,6 +19,8 @@
 		<br>
 		<br>
 
+		<h2><a href="erd.php">User Case/Interaction Flow</a></h2>
+
 
 
 	</body>
