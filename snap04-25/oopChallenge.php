4afd31b216cb2f8644b79345a06eb9ae633c6a62
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-25 09:00:18 -0600

    added lines 18 thru 25

--- snap04-25/oopChallenge.php
+++ snap04-25/oopChallenge.php
@@ -1,14 +1,11 @@
 <?php
 
-/*
- * state variables for user list sanitation
- **/
 
-private function setlist(string $newList) : void {
+public function setlist(string $newList) : void {
 	$newList = trim($newList);
 	$newList =filter_var($newList, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	if(empty($newList) === true) {
-		throw(nw\InvalidArguementException("List empty or insecure."));
+		throw(nw\InvalidArguementException("User list empty or insecure."));
 	}
 	//verify the list will fit in the database
 	if(strlen($newList) > 128) {
@@ -16,5 +13,14 @@ private function setlist(string $newList) : void {
 	}
 	// store the list
 	$this->$newList = $newList;
+}
+
+public function getList() : ?string {
+	return $this->list;
+}
+
+
+{
+	$list = new list("sdailfjweljoajjflishjriwenl");
 }
 
