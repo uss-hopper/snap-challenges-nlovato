095c660f702342a96c097913005d76194dfd2573
Natasha Lovato <nmarshlovato@cnm.edu>
2019-05-14 08:21:12 -0600

    added for each array

---
+++ snap05-14/snap05-14.js
@@ -0,0 +1,8 @@
+
+var array1 = ['hildegard.org', 'anastasia.net',
+	'ramiro.info', 'kale.biz', 'demarco.info', 'ola.org',
+	'elvis.io', 'jacynthe.com','conrad.com', 'ambrose.net'];
+
+array1 foreach(function(element) {
+	console.log(element);
+});
